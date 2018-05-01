6e09e25c91f96198d75611cce329aecef868afc6
JeniferMLlerena <jen.llerena10@gmail.com>
2018-05-01 11:10:06 -0600

    Assignment 01

--- AllEventsTicketing/Set.js
+++ AllEventsTicketing/Set.js
@@ -6,7 +6,22 @@ function Set() {
 	   var resultList = [];
        
 	   /*-------------------------------Insert your code here -------------------------------------*/
-	   
+
+		if (listA == null || listB === null) {
+			return null;
+		}
+
+		for (var i = 0; i < listA.length; i++) {
+			var nextValue = listA[i];
+
+			for (var j = 0; j < listB.length; j++) {
+				if (listB[j] === nextValue) {
+					resultList.push(listB[j]);
+					break;
+                }
+			}
+		}
+
 	   /*-------------------------------Insert your code here -------------------------------------*/
        
 	   return resultList;
@@ -19,8 +34,15 @@ function Set() {
 	   var resultList = [];
        
 	   /*-------------------------------Insert your code here -------------------------------------*/
-	   
-	   /*-------------------------------Insert your code here -------------------------------------*/ 
+		if (listA == null || listB === null) {
+			return null;
+		}
+
+        var intersection  = this.intersection(listA, listB);
+        var symetric  = this.symmetricDifference(listA, listB);
+        resultList = intersection.concat(symetric);
+
+        /*-------------------------------Insert your code here -------------------------------------*/
 	   
 	   return resultList;
 	}
@@ -33,7 +55,24 @@ function Set() {
 	   var resultList = [];
        
 	   /*-------------------------------Insert your code here -------------------------------------*/
-	   	   
+        if (listA == null || listB === null) {
+            return null;
+        }
+
+        for (var i = 0; i < listA.length; i++) {
+            var nextValue = listA[i];
+            var found = false;
+            for (var j = 0; j < listB.length; j++) {
+                if (nextValue === listB[j] ) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found === false) {
+                resultList.push(listA[i]);
+			}
+        }
+
 	   /*-------------------------------Insert your code here -------------------------------------*/
        
 	   return resultList;
@@ -43,9 +82,17 @@ function Set() {
 
 	this.symmetricDifference = function(listA, listB) {
 
-	   var resultList = [];
+		var auxList = [];
+		var resultList = [];
        
 	   /*-------------------------------Insert your code here -------------------------------------*/
+        if (listA == null || listB === null) {
+            return null;
+        }
+
+        var result1  = this.relativeComplement(listA, listB);
+        var result2  = this.relativeComplement(listB, listA);
+        resultList = result1.concat(result2);
 
 	   /*-------------------------------Insert your code here -------------------------------------*/
        
