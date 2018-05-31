7f84d36b13a885ff3aabbe580a8e111be1166ed6
JeniferMLlerena <jen.llerena10@gmail.com>
2018-05-31 11:14:04 -0600

    30/05/18 lesson-04 v2

--- cms/cms/src/app/documents/document-list/document-list.component.ts
+++ cms/cms/src/app/documents/document-list/document-list.component.ts
@@ -11,11 +11,11 @@ export class DocumentListComponent implements OnInit {
   @Output() selectedDocumentEvent: EventEmitter<Document> = new EventEmitter();
 
   documents = [
-    new Document('1', 'Doc1', 'Description1', 'htts://1.com', null),
-    new Document('2', 'Doc2', 'Description2', 'htts://2.com', null),
-    new Document('3', 'Doc3', 'Description3', 'htts://3.com', null),
-    new Document('4', 'Doc4', 'Description4', 'htts://4.com', null),
-    new Document('5', 'Doc5', 'Description5', 'htts://5.com', null)
+    new Document('1', 'Jenifer', 'lnmkllkmxklmxclkmcx', '#', null),
+    new Document('2', 'Nilagros', 'lnmkllkmxklmxclkmcx', '@', null),
+    new Document('3', 'Veronica', 'lnmkllkmxklmxclkmcx<', '#', null),
+    new Document('4', 'Llerena', 'lnmkllkmxklmxclkmcx<', '#', null),
+    new Document('5', 'Paye', 'lnmkllkmxklmxclkmcx', '#', null)
   ];
 
   constructor() { }
